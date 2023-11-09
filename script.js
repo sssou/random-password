c9caf67058c3e224fde54aa0ae7b393feb9c3edc
sssou <souhardachanda@Souharda-Chanda.local>
2023-11-09 12:48:15 +0000

    prompts for passsword options and random array function added

--- script.js
+++ script.js
@@ -88,16 +88,38 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+// Object containing password criteria
+var crit = {
+  passlength: 0,
+  lowercase: false,
+  uppercase: false,
+  numeric: false,
+  special: false,
+}
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-  
-
+  // user is forced to enter number between 8 and 128 (can still enter a wrong data type)
+  while ((crit.passlength < 8) || (crit.passlength > 128)) {
+      crit.passlength = parseInt(prompt("Enter the length of password (8 - 128 characters)"));
+  }
+  // user is forced to enter yes for atleast 1 char type
+  while ((crit.lowercase === false) && (crit.uppercase === false) && (crit.numeric === false) && (crit.special === false)) {
+    alert("you must choose atleast one of the 4 character types (recomended to choose all 4)")
+    crit.lowercase = confirm("would you like lowercased characters?")
+    crit.uppercase = confirm("would you like uppercased characters?")
+    crit.numeric = confirm("would you like numeric characters?")
+    crit.special = confirm("would you like special characters?")
+  }
 }
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  var i = Math.floor(Math.random() * arr.length)
+  return (arr[i])
 }
+
 
 // Function to generate password with user input
 function generatePassword() {
